4be150301258e24126f50b8601ace5f48d7b1585
pinduxs <168519209+pinduxs@users.noreply.github.com>
2024-05-28 17:43:49 +0300

    modified:   end.html 	modified:   game.js 	modified:   index.html

--- game.js
+++ game.js
@@ -102,7 +102,7 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         //go to the end page
-        return window.location.assign('/end.html');
+        return window.location.assign('/randomindex.html');
     }
    
     questionCounter++;    // skaita juatajumu uz prieksu 
